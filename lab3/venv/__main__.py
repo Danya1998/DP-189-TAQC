64f51eae115b2def707e71e14cc118c30f8b948c
Danya1998 <yurchenkodanil1998@gmail.com>
2020-06-07 18:30:13 +0300

    Program will return the word representation of input number

---
+++ lab3/venv/__main__.py
@@ -0,0 +1,4 @@
+from venv.nums_into_words import num_into_words
+
+if __name__ == '__main__':
+    num_into_words()
---
+++ lab3/venv/nums_into_words.py
@@ -0,0 +1,5 @@
+from num2words import num2words
+
+def num_into_words():
+    ''' The function will return the word representation of input number'''
+    print(num2words(input("Enter a number: ")))
