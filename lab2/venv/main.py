72fdcd79eb6289d3c78ce15a6a80f774889d53f5
Danya1998 <yurchenkodanil1998@gmail.com>
2020-06-09 13:22:43 +0300

    Program which count symbols in file

--- lab2/venv/main.py
+++ lab2/venv/main.py
@@ -1,43 +1,42 @@
 import re
+import os.path
+from functools import wraps
 from contextlib import contextmanager
-from typing import Tuple, Generator, TextIO
+from typing import Tuple, TextIO
 
-@contextmanager
-def file_open() ->Generator:
-    """Function decorator which opens file for reading"""
-    try:
-        filename = input("Enter the name of the file:")
-        file_open = open(filename, 'r')
-        yield file_open
-    except OSError:
-        print("We had an error!")
-    finally:
-        file_open.close()
 
 def counter(filename:str) ->Tuple[int,int,int,int,int]:
-    '''Funcion which counts symbols according to given condition'''
-    with file_open()  as filename:
-        lines = 0
-        empty_str = 0
-        line_z=0
-        letter_z=0
-        line_and=0
-        for line in filename:
-            lines += 1
-            if (line == "\n" or line == "\r\n"): empty_str += 1
-            if re.search('z',line): line_z+=1
-            letter_z+=line.count('z')
-            if re.search('and',line): line_and +=1
+    '''Funcion which counts needed symbols according to given condition'''
+    lines = 0
+    empty_str = 0
+    line_z=0
+    letter_z=0
+    line_and=0
+    for line in filename:
+        lines += 1
+        if (line == "\n" or line == "\r\n"): empty_str += 1
+        if re.search('z',line): line_z+=1
+        letter_z+=line.count('z')
+        if re.search('and',line): line_and +=1
     return lines,empty_str,line_z,letter_z,line_and
 
 def main() -> None:
-    filename = file_open()
-    res = counter(filename)
-    print("Total lines:", res[0])
-    print("Empty lines:", res[1])
-    print("Lines with z:", res[2])
-    print("Z count:", res[3])
-    print("Lines with and:", res[4])
+    while(True):
+        filename = input("Enter the name of the file:")
+        check_file = os.path.exists(filename)
+        if check_file:
+            file_open = open(filename, 'r')
+            break
+        else:
+            print("We had an error!")
+    result = counter(file_open)
+    print("Total lines:", result[0])
+    print("Empty lines:", result[1])
+    print("Lines with z:", result[2])
+    print("Z count:", result[3])
+    print("Lines with and:", result[4])
+    file_open.close()
+
 
 if __name__ == '__main__':
     main()
