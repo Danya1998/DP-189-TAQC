6925a4438b81dd186a2ebf14ea358fe2e728a71f
Danya1998 <yurchenkodanil1998@gmail.com>
2020-06-22 13:23:08 +0300

    Update fibo.py

--- elementaryTask/elem8/venv/fibo.py
+++ elementaryTask/elem8/venv/fibo.py
@@ -1,18 +1,52 @@
-def main():
-    print(fibo_mass())
+import math
 
-def fibo_mass():
-    min_value = int(input("Choose min value of Fibo-diapasone:"))
-    max_value = int(input("Choose max value of Fibo-diapasone:"))
-    fibo_arr = [0,1]
-    fibo_res=[]
-    sum_fibo=0
-    for i in range(2,50):
-        sum_fibo= fibo_arr[i-1]+fibo_arr[i-2]
-        fibo_arr.append(sum_fibo)
-        if (min_value<sum_fibo<=max_value): fibo_res.append(sum_fibo)
-    return fibo_res
+
+def fibo_length(len_el):
+    fib1, fib2 = 0, 1
+    fibo_result = []
+    if len_el == 1:
+        fibo_result.append(fib1)
+        fibo_result.append(fib2)
+    for elem in range(0, pow(10, len_el)):
+        fib_sum = fib1 + fib2
+        fib1 = fib2
+        fib2 = fib_sum
+        if len(str(fib2)) == len_el:
+            fibo_result.append(fib2)
+        elif len(str(fib2)) > len_el:
+            break
+        else:
+            pass
+    return fibo_result
+
+
+def fibo_range(min_value, max_value):
+    fibo_result = []
+    fib1, fib2 = 0, 1
+    if min_value == 0:
+        fibo_result.append(fib1)
+        fibo_result.append(fib2)
+    for elem in range(min_value, max_value):
+        fib_sum = fib1 + fib2
+        fib1 = fib2
+        fib2 = fib_sum
+        if (min_value <= fib_sum <= max_value):
+            fibo_result.append(fib2)
+    return fibo_result
+
+
+def main():
+    choose_mode = int(input(f"Choose in which mode you want to search fibo sequence:\n"
+                            "1 - in certain range\n"
+                            "2 - length of elements\n"))
+    if choose_mode == 1:
+        min_value = int(input("Choose min value of Fibo-diapasone:"))
+        max_value = int(input("Choose max value of Fibo-diapasone:"))
+        print(fibo_range(min_value, max_value))
+    else:
+        len_el = int(input("Enter length of elem:"))
+        print(fibo_length(len_el))
+
 
 if __name__ == '__main__':
     main()
-
